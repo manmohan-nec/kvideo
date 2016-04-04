633464be07a0704f4c16320bddec9f21cf78907c
manmohan-nec <manmohan.menon@nectechnologies.in>
2016-04-04 17:03:28 +0530

    fixed changes and uploading working code in kibana4 app

--- public/kvideo.js
+++ public/kvideo.js
@@ -1,37 +1,29 @@
 define(function(require) {
   require('plugins/kvideo/kvideo.css');
   require('angular');  
-  require('ng-video/dist/ng-video');  
-  //require('plugins/kvideo/default');    
-  const APP_NAME = 'myApp';
+  require('ng-video/dist/ng-video');
 
-  (function Default($angular) {      
-    $angular.module(APP_NAME, ['ngVideo']);
-  })(window.angular);
-  var module = require('ui/modules').get('kvideo');
-  //testing this plugin1
-  module = (function VideoController($angular) {    
-    $angular.module(APP_NAME).controller('VideoController',function videoController($scope, video) {
-        $scope.playlistOpen = false;
-        $scope.videos = {
-            first:  '/home/test/Downloads/SampleVideo_1280x720_1mb.mp4',
-            second: '/home/test/Downloads/SampleVideo_1280x720_2mb.mp4'
+  var module = require('ui/modules').get('kvideo',['ngVideo']);
+  
+  module.controller('VideoCtrl', ['$scope', 'video', function($scope, video) {     
+    $scope.playlistOpen = false;
+    $scope.videos = {
+            first:  'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+            second: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4'
         };
-        $scope.playVideo = function playVideo(sourceUrl) {
+    $scope.playVideo = function playVideo(sourceUrl) {
             video.addSource('mp4', sourceUrl, true);
         };        
-        $scope.getVideoName = function getVideoName(videoModel) {
+    $scope.getVideoName = function getVideoName(videoModel) {
             switch (videoModel.src) {
                 case ($scope.videos.first): return "Big Buck Bunny";
                 case ($scope.videos.second): return "The Bear";
                 default: return "Unknown Video";
             }
         };
-        video.addSource('mp4', $scope.videos.first);
-        video.addSource('mp4', $scope.videos.second);
-    });
-  })(window.angular);
-
+    video.addSource('mp4', $scope.videos.first);
+    video.addSource('mp4', $scope.videos.second);
+ }]);  
   function VideoProvider(Private) {
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
     return new TemplateVisType({
@@ -40,13 +32,9 @@ define(function(require) {
       icon: 'fa-camera-retro',
       description: 'Display random video on kibana dashboard',
       requiresSearch: false,
-      template: require('plugins/kvideo/kvideo.html')
-      // params: {
-      //   //editor: require('plugins/kvideo/kvideo-editor.html')        
-      // }      
+      template: require('plugins/kvideo/kvideo.html')            
     });
-  }
-  
+  }  
   require('ui/registry/vis_types').register(VideoProvider);  
   return VideoProvider;
 });
